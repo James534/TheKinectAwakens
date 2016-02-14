14f96c07f01b329e0218bb72e3b34859b2480798
apalm3 <apalm3@gatech.edu>
2016-02-13 22:23:16 -0800

    Add lightsaber and on/off functionality

---
+++ TKA/Assets/Lets Make a Lightsaber/Prefabs/lightsabers.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightsabers : MonoBehaviour {
+
+	LineRenderer lineRend;
+	public Transform startPos;
+	public Transform endPos;
+	private bool on = true;
+	private Vector3 endPositionExtended;
+
+	private float textureOffset = 0f;
+	// Use this for initialization
+	void Start () {
+		lineRend = GetComponent<LineRenderer>();
+		endPositionExtended = endPos.localPosition;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//update line positions
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (on) {
+				on = false;
+			} else {
+				on = true;
+			}
+		}
+
+		if (on) {
+			endPos.localPosition = Vector3.Lerp (endPos.localPosition, endPositionExtended, Time.deltaTime * 5f);
+		} else {
+			endPos.localPosition = Vector3.Lerp (endPos.localPosition, startPos.localPosition, Time.deltaTime*5f);
+		}
+
+
+		lineRend.SetPosition(0, startPos.position);
+		lineRend.SetPosition(1, endPos.position);
+
+		textureOffset -= Time.deltaTime * 2f;
+
+		if (textureOffset < -10f) {
+			textureOffset += 10f;
+
+		}
+		lineRend.sharedMaterials [1].SetTextureOffset ("_MainTex", new Vector2(textureOffset, 0f));
+	}
+}
---
+++ TKA/Assets/lightsaber.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightsaber : MonoBehaviour {
+
+	LineRenderer lineRend;
+	public Transform startPos;
+	public Transform endPos;
+
+	private float textureOffset = 0f;
+	// Use this for initialization
+	void Start () {
+		lineRend = GetComponent<LineRenderer>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//update line positions
+		lineRend.SetPosition(0, startPos.position);
+		lineRend.SetPosition(1, endPos.position);
+
+		textureOffset -= Time.deltaTime * 2f;
+
+		if (textureOffset < -10f) {
+			textureOffset += 10f;
+	
+		}
+		lineRend.sharedMaterials [1].SetTextureOffset ("_MainTex", new Vector2(textureOffset, 0f));
+	}
+}
